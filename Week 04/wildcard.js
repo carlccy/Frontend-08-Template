5c77a28c5bb84a497475f24c9c9cd20ae4abd271
chencheng <chencheng@bitforex.com>
2021-01-17 21:01:00 +0800

    week04

---
+++ Week 04/wildcard.js
@@ -0,0 +1,59 @@
+// 只有 * 最后一个*和其他的*不一样，最后一个*是尽量多匹配，前面的是尽量少多匹配
+// 只有 ？
+function find (source, pattern) {
+
+  let startCount = 0 // * 个数
+  for (let i = 0; i < pattern.length; i++) {
+    if (pattern[i] === '*') {
+      startCount ++
+    }
+  }
+
+  if (startCount === 0) { // 没有 * 号，一一匹配
+    for (let i = 0; i < pattern.length; i++) {
+      if (pattern[i] !== source[i] && pattern[i] !== '?') {
+        return false
+      }
+      return
+    }
+  }
+
+  let i = 0 // pattern 位置
+  let lastIndex = 0 // source 位置
+
+  for (i = 0; pattern[i] !== '*'; i++) {
+    if (pattern[i] !== source[i] && pattern[i] !== '?') {
+      return false
+    }
+  }
+
+  lastIndex = i
+
+  // 匹配最后一个 * 前面
+  for (let p = 0; p < startCount - 1; p++) {
+    i++
+    let subPattern = '' // 最后一个 * 号后边的格式，都替换成正则都表达式
+    while (pattern[i] !== '*') {
+      subPattern += pattern[i]
+      i++
+    }
+
+    // [\\s\\S] 任意字符串
+    let reg = new RegExp(subPattern.replace(/\?/g, '[\\s\\S]'), 'g')
+    reg.lastIndex = lastIndex // 接着之前都工作去找
+
+    console.log(reg.exec(source))
+
+    lastIndex = reg.lastIndex
+  }
+
+  // 匹配 * 号尾部
+  for (let j = 0; j < source.length - lastIndex && pattern[pattern.length - j] !== '*'; j++) {
+    if (pattern[pattern.length - j] !== source[source.length - j]
+      && pattern[pattern.length - j] !== '?') {
+        return false
+    }
+  }
+
+  return true
+}
